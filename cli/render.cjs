4f86db8776882de160e11c30822ea4ce7e246f16
Romello Goodman <romellogood@gmail.com>
2022-06-20 00:34:34 -0400

    saving progress on render

--- cli/render.cjs
+++ cli/render.cjs
@@ -1,7 +1,74 @@
-const command = (filenameParam, options) => {
-  // TODO: use open pkg to open file viewer to .atwo/output/$sketch
+const express = require("express");
+const fs = require("fs");
+const open = require("open");
+const path = require("path");
+const puppeteer = require("puppeteer");
+const webpack = require("webpack");
+const webpackDevMiddleware = require("webpack-dev-middleware");
 
-  console.log("render", filename, options);
+const { getSketch } = require("./common/libraries.cjs");
+const { getWebpackConfig } = require("./common/webpack.cjs");
+
+const command = async (filenameParam, options) => {
+  const port = 8080;
+  const url = `http://localhost:${port}`;
+  const app = express();
+
+  app.use(
+    "/public",
+    express.static(path.resolve(__dirname, "./assets/public"))
+  );
+
+  const sketch = getSketch(filenameParam);
+  const webpackConfig = getWebpackConfig(sketch, {
+    filename: "[name].js",
+    mode: "production",
+  });
+  const compiler = await webpack(webpackConfig);
+
+  console.log("compile start");
+
+  await new Promise((resolve) => {
+    compiler.run((err, stats) => {
+      // console.log("compile end", stats);
+      console.log("compile end");
+      resolve();
+
+      compiler.close((closeErr) => {});
+    });
+  });
+
+  // app.use(
+  //   webpackDevMiddleware(compiler, {
+  //     publicPath: webpackConfig.output.publicPath,
+  //   })
+  // );
+
+  app.listen(port, (error) => {
+    if (error) {
+      console.log(error);
+    }
+
+    console.log(`server listening at: ${url}`);
+  });
+
+  const { number, seed = "foo" } = options;
+  const sketchUrl = `${url}/${filenameParam}.html?seed=${seed}`;
+  const folderPath = path.join(process.cwd(), "output");
+  const savePath = path.join(folderPath, `${filenameParam}.png`);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(sketchUrl);
+
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+  }
+
+  await page.screenshot({ path: savePath });
+  await browser.close();
+
+  open(folderPath);
 };
 
 module.exports = command;
